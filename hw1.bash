eb7cc57f933c202b10288f32481b6fb8d0914e0e
Joseph Yuanhao Li <Josephl.b612@gmail.com>
2021-11-01 18:06:41 -0400

    edit hw1.bash

--- hw1.bash
+++ hw1.bash
@@ -8,3 +8,10 @@
 #SBATCH --ntasks-per-node=8
 #SBATCH --mem=16Gb
 #SBATCH --exclusive
+
+module load anaconda3/3.7
+workls=/home/li.yuanhao1/tmp7105/homework1
+cd /home/li.yuanhao1/tmp7105/homework1
+export OMP_NUM_THREADS=4
+./omp_helloc
+srun python hw1.py
